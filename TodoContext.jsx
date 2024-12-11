0df6653021028c28276053035980c5cd0ac5a5ab
Joydeepkar1234 <168506863+Joydeepkar1234@users.noreply.github.com>
2024-12-11 14:56:56 +0530

    Update TodoContext.jsx

--- TodoContext.jsx
+++ TodoContext.jsx
@@ -4,13 +4,11 @@ export const TodoContext = createContext();
 
 export const TodoProvider = (props) => {
   const [todos, setTodos] = useState(() => {
-    // Load from Local Storage
     const storedTodos = localStorage.getItem("todos");
     return storedTodos ? JSON.parse(storedTodos) : [];
   });
 
   useEffect(() => {
-    // Save to Local Storage whenever `todos` changes
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
